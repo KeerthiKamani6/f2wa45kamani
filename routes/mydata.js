5a0f046434ee655898b4cc85c546b78c6d25ead7
KeerthiKamani6 <92873846+KeerthiKamani6@users.noreply.github.com>
2022-10-26 21:05:00 -0500

    updated

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'venkata keerthi kameni' });
+  res.render('mydata', { title: 'venkata keerthi kamani' });
 });
 
 module.exports = router;
