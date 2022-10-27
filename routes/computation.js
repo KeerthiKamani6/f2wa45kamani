343e05c8179ff03f5f2525e91bfd0ea8785441f5
KeerthiKamani6 <92873846+KeerthiKamani6@users.noreply.github.com>
2022-10-26 22:07:35 -0500

    updated

--- routes/computation.js
+++ routes/computation.js
@@ -3,9 +3,9 @@ var express = require('express');
 var router = express.Router();
 var x = Math.random();
 var y = Math.random();
-var val1="Math.cos(base)applied to "+ x + " is "+ Math.sign(x)
-var val2="Math.asin()applied to "+ x + " is "+ Math.sign(x)
-var val3="Math.asinh() applied to "+ x + " is "+ Math.tanh(x);
+var val1="Math.cos(base)applied to "+ x + " is "+ Math.cos(x)
+var val2="Math.asin()applied to "+ x + " is "+ Math.asin(x)
+var val3="Math.asinh() applied to "+ x + " is "+ Math.asinh(x);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('computation', { title:'VENKATA KEERTHI KAMANI ',value1: val1, value2:vasl2, value3:val3 });
